f3e2aff508ab160a78242714b97febb67dc7fcef
farisqlail <faris.riskilail@gmail.com>
2024-06-10 15:05:23 +0700

    feat: button scroll to top

--- pages/restaurant/detail/[...slug].vue
+++ pages/restaurant/detail/[...slug].vue
@@ -3,6 +3,37 @@
     <div>
       <section id="home">
         <div class="frame" :class="!products || !tableCode ? 'hidden' : ''">
+          <button
+            v-show="showScrollButton"
+            @click="scrollToTop"
+            class="scroll-to-top"
+          >
+            <svg
+              fill="#ffffff"
+              height="50px"
+              width="50px"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 330 330"
+              xml:space="preserve"
+              stroke="#ffffff"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  id="XMLID_224_"
+                  d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394 l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393 C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"
+                ></path>
+              </g>
+            </svg>
+          </button>
           <!-- carousel -->
           <Navbar :to="navbarTo" />
           <NuxtLazyHydrate>
@@ -302,7 +333,7 @@
         </div>
       </section>
 
-      <div class="flex justify-center ">
+      <div class="flex justify-center">
         <div class="all-items-text">Semua item sudah dimunculkan</div>
       </div>
       <div class="flex justify-center">
@@ -350,6 +381,7 @@ export default defineComponent({
   },
   data() {
     return {
+      showScrollButton: false,
       navbarTo: "/",
       isHidden: true,
       isUseTable: false,
@@ -522,11 +554,21 @@ export default defineComponent({
     if (process.client) {
       this.getCartItems();
     }
+    window.addEventListener("scroll", this.handleScroll);
   },
-  beforeCreate() {
-    // console.log('beforeCreate')
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
   methods: {
+    handleScroll() {
+      this.showScrollButton = window.scrollY > 200;
+    },
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
     fetchProducts() {
       // Simulasi pengambilan data
       this.isLoading = "open";
@@ -787,3 +829,4 @@ export default defineComponent({
   },
 });
 </script>
+
