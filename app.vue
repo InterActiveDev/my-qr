65de3c751938c234e03dcb5e346b8903ee083452
farisqlail <faris.riskilail@gmail.com>
2024-06-11 14:47:13 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -31,9 +31,7 @@ export default {
     };
   },
   mounted() {
-  document.addEventListener('touchmove', function (event) {
-    if (event.scale !== 1) { event.preventDefault(); }
-  }, { passive: false });
+ document.addEventListener('touchmove', this.preventPinchZoom, { passive: false });
     document.addEventListener("contextmenu", this.preventContextMenu);
   },
   beforeDestroy() {
@@ -51,6 +49,11 @@ export default {
     },
   },
   methods: {
+    preventPinchZoom(event) {
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
+    },
     preventGesture(event) {
       event.preventDefault();
     },
